73dce6de7cd2983d8739749b66d72d7e65d3e876
qzhang16 <qzhang16@ra.rockwell.com>
2023-08-17 14:51:34 +0800

    sharedConsumer will have only one of the subscribers consume the published message.

--- src/main/java/com/asg/p2s01/HRApp.java
+++ src/main/java/com/asg/p2s01/HRApp.java
@@ -1,6 +1,7 @@
 package com.asg.p2s01;
 
 import javax.jms.JMSContext;
+import javax.jms.JMSProducer;
 import javax.jms.Topic;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -18,17 +19,30 @@ public class HRApp {
                     "admin");
                     JMSContext jmsContext = cf.createContext()) {
 
-                Employee emp = new Employee();
-                emp.setId(1);
+                Employee emp = null;
+
+                JMSProducer producer = jmsContext.createProducer();
+
+                for (int i = 0; i < 10; i++) {
+                    emp = new Employee();
+                    emp.setId(i);
+                    emp.setName("Joe" + i);
+                    emp.setDesignation("Engineer");
+
+                    // ObjectMessage msg = jmsContext.createObjectMessage(emp);
+                    producer.send(empTopic, emp);
+                    System.out.println("sent " + emp.getName() + " to topic");
+
+                }
+
+                emp = new Employee();
+                emp.setId(1000);
                 emp.setName("Bob");
                 emp.setDesignation("Engineer");
 
                 // ObjectMessage msg = jmsContext.createObjectMessage(emp);
                 jmsContext.createProducer().send(empTopic, emp);
-
                 System.out.println("sent " + emp.getName() + " to topic");
-
-                
 
             }
 
--- src/main/java/com/asg/p2s01/PayrollApp.java
+++ src/main/java/com/asg/p2s01/PayrollApp.java
@@ -1,13 +1,21 @@
 package com.asg.p2s01;
 
+import java.util.concurrent.CountDownLatch;
+
+import javax.jms.JMSConsumer;
 import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
 import javax.jms.Topic;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
-public class PayrollApp {
+public class PayrollApp implements MessageListener {
+    private static final CountDownLatch latch = new CountDownLatch(1);
+
     public static void main(String[] args) {
 
         try {
@@ -20,12 +28,16 @@ public class PayrollApp {
 
                 // ObjectMessage msg = jmsContext.createObjectMessage(emp);
                 // jmsContext.createProducer().send(empTopic, emp);
-                Employee emp = jmsContext.createConsumer(empTopic).receiveBody(Employee.class);
+                // Employee emp = jmsContext.createConsumer(empTopic).receiveBody(Employee.class);
+                JMSConsumer consumer = jmsContext.createSharedConsumer(empTopic,"paytopic");
+                consumer.setMessageListener(new PayrollApp());
 
-                System.out.println("Payroll: received " + emp.getName() + " from topic");
+                latch.await();
 
-                
+                // System.out.println("Payroll: received " + emp.getName() + " from topic");
 
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
         } catch (NamingException e) {
@@ -33,5 +45,17 @@ public class PayrollApp {
         }
 
     }
-    
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            System.out.println("Payroll " + Thread.currentThread().getId() + " : received "
+                    + message.getBody(Employee.class).getName() + " from topic");
+                    if (message.getBody(Employee.class).getName().equalsIgnoreCase("Bob")) latch.countDown();
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
